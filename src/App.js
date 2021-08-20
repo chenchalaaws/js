d47ab45b8623a787fe7404322e9bdc418d5e4e7f
unknown <chenchala.srinu@gmai.com>
2021-08-20 17:53:03 +0530

    latest

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>welcome...</h1>
+     <h5>this is empity project</h5>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/demo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+function Demo(){
+return(<div>
+    <div className="row">
+        <div className="col-md-3 offset-md-1 text-right">
+            F name:
+        </div>
+        <div className="col-md-4"><input type="text" </div>
+    </div>
+</div>)
+}
+export default Demo
